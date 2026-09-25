c51810a857ebd95b0bfb65e556f07dca9354fd9a
Yoshitaka Osawa <yoshitakaosawa@YoshitakanoMacBook-Air.local>
2026-09-26 02:17:19 +0900

    fix: import missing useCallback in AdminExamEditor

--- src/pages/AdminExamEditor.jsx
+++ src/pages/AdminExamEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import { useParams, useNavigate, Link, useLocation } from 'react-router-dom';
 import { getAdminExamById, saveAdminExam, updateAdminFields, uploadExamPdf } from '../services/adminExamService';
